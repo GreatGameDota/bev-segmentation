49693139bf968b2cbd2eaa413be3d84f3dbf74b3
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2022-06-28 22:51:04 +0000

    Add download_all.sh

--- download/data_2d_download.sh
+++ download/data_2d_download.sh
@@ -1,16 +1,6 @@
 
-train_list=(
-      # "2013_05_28_drive_0000_sync"
-            "2013_05_28_drive_0002_sync" 
-	    # "2013_05_28_drive_0003_sync"
-      #       "2013_05_28_drive_0004_sync" 
-      #       "2013_05_28_drive_0005_sync" 
-      #       "2013_05_28_drive_0006_sync" 
-      #       "2013_05_28_drive_0007_sync" 
-      #       "2013_05_28_drive_0009_sync" 
-	    # "2013_05_28_drive_0010_sync"
-      )
-cam_list=("00")
+train_list="$1"
+cam_list="00"
 
 root_dir=KITTI-360
 data_2d_dir=data_2d_raw
@@ -21,8 +11,8 @@ mkdir -p $root_dir/$data_2d_dir
 cd $root_dir 
 
 # perspective images
-for sequence in ${train_list[@]}; do
-    for camera in ${cam_list[@]}; do 
+for sequence in ${train_list}; do
+    for camera in ${cam_list}; do 
 	zip_file=${sequence}_image_${camera}.zip
         curl https://s3.eu-central-1.amazonaws.com/avg-projects/KITTI-360/data_2d_raw/${zip_file} -o ${zip_file}
 	unzip -d ${data_2d_dir} ${zip_file} 
--- download/data_3d_raw_download.sh
+++ download/data_3d_raw_download.sh
@@ -1,15 +1,5 @@
 
-train_list=(
-          # "2013_05_28_drive_0000_sync"
-            "2013_05_28_drive_0002_sync" 
-	    # "2013_05_28_drive_0003_sync"
-      #       "2013_05_28_drive_0004_sync" 
-      #       "2013_05_28_drive_0005_sync" 
-      #       "2013_05_28_drive_0006_sync" 
-      #       "2013_05_28_drive_0007_sync" 
-      #       "2013_05_28_drive_0009_sync" 
-	    # "2013_05_28_drive_0010_sync"
-      )
+train_list="$1"
 
 root_dir=KITTI-360
 data_3d_dir=data_3d_raw
@@ -20,7 +10,7 @@ mkdir -p $root_dir/$data_3d_dir
 cd $root_dir 
 
 # 3d scans
-for sequence in ${train_list[@]}; do
+for sequence in ${train_list}; do
     zip_file=${sequence}_velodyne.zip
     curl https://s3.eu-central-1.amazonaws.com/avg-projects/KITTI-360/data_3d_raw/${zip_file} -o ${zip_file}
     unzip -d ${data_3d_dir} ${zip_file} 
